1a28a8e511331013a5182a698c255beabc34c791
zl <zhaluobox@163.com>
2022-09-30 14:27:46 +0800

    集成nacos 测试demo

---
+++ fi-example/fi-example-nacos/src/main/java/org/framework/integration/example/nacos/NacosApplication.java
@@ -0,0 +1,14 @@
+package org.framework.integration.example.nacos;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created  on 2022/9/30 13:13:45
+ *
+ * @author zl
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosApplication {
+}
