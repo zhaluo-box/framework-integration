e85275b5eb5f256ee708828f50ff51501e0a9272
zl <zhaluobox@163.com>
2023-04-18 17:50:08 +0800

    add ruoyi-cloud learn vue3

---
+++ ruoyi/ruoyi-auth/src/main/java/com/ruoyi/auth/form/LoginBody.java
@@ -0,0 +1,23 @@
+package com.ruoyi.auth.form;
+
+import lombok.Data;
+
+/**
+ * 用户登录对象
+ *
+ * @author ruoyi
+ */
+@Data
+public class LoginBody {
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 用户密码
+     */
+    private String password;
+
+}
